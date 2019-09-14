cb4d8d2466d4167855b071add52826a1d0dd6bab
Art Goldhammer <art.goldhammer@gmail.com>
2019-09-14 16:23:33 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to moz
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/moz/core.clj
@@ -0,0 +1,33 @@
+(ns moz.core
+  (:require [oz.core :as oz])
+  (:gen-class))
+
+(defn play-data [& names]
+  (for [n names
+        i (range 20)]
+    {:time i :item n :quantity (+ (Math/pow (* i (count n)) 0.8) (rand-int (count n)))}))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(def line-plot
+  {:data {:values (play-data "monkey" "slipper" "broom")}
+   :encoding {:x {:field "time"}
+              :y {:field "quantity"}
+              :color {:field "item" :type "nominal"}}
+   :mark "line"})
+
+(def stacked-bar
+  {:data {:values (play-data "munchkin" "witch" "dog" "lion" "tiger" "bear")}
+   :mark "bar"
+   :encoding {:x {:field "time"
+                  :type "ordinal"}
+              :y {:aggregate "sum"
+                  :field "quantity"
+                  :type "quantitative"}
+              :color {:field "item"
+                      :type "nominal"}}})
+
+(oz/view! stacked-bar)
